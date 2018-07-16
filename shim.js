ffdbdfd8750df5f7c0e1c970e2eb68e913659d57
Ace Nassri <anassri@google.com>
2018-07-16 11:02:17 -0700

    Use shim instead of emulator

--- shim.js
+++ shim.js
@@ -1,5 +1,5 @@
 /**
- * Copyright 2018, Google LLC
+ * Copyright 2018, Google LLC.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -13,7 +13,7 @@
  * limitations under the License.
  */
 
-const httpShim = (PORT) => {
+const httpShim = (PORT, timeout) => {
   // [START functions_testing_shim_http]
   // Import dependencies
   const gcfCode = require('./index.js');
@@ -22,6 +22,9 @@ const httpShim = (PORT) => {
 
   // TODO(developer): specify the port to use
   // const PORT = 3000;
+
+  // TODO(developer): optionally specify a timeout in ms
+  // const timeout; // = 60000
 
   // Start local HTTP server
   const app = express();
@@ -42,10 +45,17 @@ const httpShim = (PORT) => {
     app.get(`/${gcfFn}`, handler);
     app.post(`/${gcfFn}`, handler);
   });
+
+  // Handle timeout
+  if (timeout) {
+    setTimeout(() => {
+      server.close();
+    }, timeout);
+  }
   // [END functions_testing_shim_http]
 };
 
-const pubsubShim = (gcfFn, topicName, subscriptionName) => {
+const pubsubShim = (gcfFn, topicName, subscriptionName, timeout) => {
   // [START functions_testing_shim_pubsub]
   // Import dependencies
   const Pubsub = require('@google-cloud/pubsub');
@@ -59,21 +69,36 @@ const pubsubShim = (gcfFn, topicName, subscriptionName) => {
   // const topicName = process.env.TOPIC || 'YOUR_TOPIC';
   // const subscriptionName = process.env.SUBSCRIPTION || 'YOUR_SUBSCRIPTION';
 
+  // TODO(developer): optionally specify a timeout in ms
+  // const timeout; // = 60000
+
   // Subscribe to Pub/Sub topic
   const subscription = pubsub.topic(topicName).subscription(subscriptionName);
 
   // Handle a single Pub/Sub message
   const messageHandler = (msg) => {
-    gcfFn({ data: msg }, () => {
+    const cb = () => {
       msg.ack();
       subscription.removeListener(`message`, messageHandler);
-    });
+    };
+    if (process.version.includes('v6.')) {
+      gcfFn({ data: msg }, cb);
+    } else {
+      Promise.resolve(gcfFn({ data: msg }, {})).then(cb);
+    }
   };
   subscription.on(`message`, messageHandler);
+
+  // Handle timeout
+  if (timeout) {
+    setTimeout(() => {
+      subscription.removeListener(`message`, messageHandler);
+    }, timeout);
+  }
   // [END functions_testing_shim_pubsub]
 };
 
-const storageShim = (gcfFn, bucketName, topicName, subscriptionName) => {
+const storageShim = (gcfFn, bucketName, topicName, subscriptionName, timeout) => {
   // [START functions_testing_shim_storage]
   // Import dependencies
   const Pubsub = require('@google-cloud/pubsub');
@@ -92,6 +117,9 @@ const storageShim = (gcfFn, bucketName, topicName, subscriptionName) => {
   // const topicName = process.env.TOPIC || 'YOUR_TOPIC';
   // const subscriptionName = process.env.SUBSCRIPTION || 'YOUR_SUBSCRIPTION';
 
+  // TODO(developer): optionally specify a timeout in ms
+  // const timeout; // = 60000
+
   // Create notification on target bucket
   // Further info: https://cloud.google.com/storage/docs/reporting-changes
   const bucket = storage.bucket(bucketName);
@@ -106,13 +134,26 @@ const storageShim = (gcfFn, bucketName, topicName, subscriptionName) => {
       // Handle a single Pub/Sub message
       const messageHandler = (msg) => {
         const data = JSON.parse(Buffer.from(msg.data, 'base64').toString());
-        gcfFn({ data: data }, () => {
+        const cb = () => {
           msg.ack();
           subscription.removeListener(`message`, messageHandler);
           resolve(notification);
-        });
+        };
+        if (process.version.includes('v6.')) {
+          gcfFn(data, cb);
+        } else {
+          Promise.resolve(gcfFn(data, {})).then(cb);
+        }
       };
       subscription.on(`message`, messageHandler);
+
+      // Handle timeout
+      if (timeout) {
+        setTimeout(() => {
+          subscription.removeListener(`message`, messageHandler);
+          resolve(notification);
+        }, timeout);
+      }
     }))
     .then(notification => notification.delete()); // Delete notification
   // [END functions_testing_shim_storage]
@@ -125,7 +166,7 @@ require(`yargs`) // eslint-disable-line
     'http <port>',
     'HTTP-triggered-function shim',
     {},
-    opts => httpShim(opts.port)
+    opts => httpShim(opts.port, opts.timeout)
   )
   .command(
     'pubsub <functionName> <topic> <subscription>',
@@ -134,7 +175,8 @@ require(`yargs`) // eslint-disable-line
     opts => pubsubShim(
       gcfCodeGlobal[opts.functionName],
       opts.topic,
-      opts.subscription
+      opts.subscription,
+      opts.timeout
     )
   )
   .command(
@@ -145,9 +187,15 @@ require(`yargs`) // eslint-disable-line
       gcfCodeGlobal[opts.functionName],
       opts.bucket,
       opts.topic,
-      opts.subscription
+      opts.subscription,
+      opts.timeout
     )
   )
+  .option('t', {
+    alias: 'timeout',
+    requiresArg: true,
+    type: 'number'
+  })
   .wrap(120)
   .help()
   .strict()
